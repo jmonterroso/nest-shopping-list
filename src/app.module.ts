ccab5d29592d839862aafa9a65cf59812109ced9
jayson.monterroso <jayson.monterroso@gmail.com>
2020-11-23 10:03:27 -0600

    updating connection string

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,13 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { ConfigModule } from 'nestjs-config';
 import * as path from 'path';
 
+
 @Module({
   // imports: [MongooseModule.forRoot('mongodb://ad:X3l4ju.mc@ds211558.mlab.com:11558/4geeks'), ShoppingListModule],
   imports: [
     ConfigModule.load(path.resolve(__dirname, 'config', '**/!(*.d).{ts,js}')),
     ShoppingItemModule, ShoppingListModule,
-    MongooseModule.forRoot('mongodb://ad:X3l4ju.mc@ds211558.mlab.com:11558/4geeks'),
+    MongooseModule.forRoot('mongodb+srv://shopping_list_db:X3l4ju.mc1234@4geeks.qssff.mongodb.net/<dbname>?retryWrites=true&w=majority'),
     // MongooseModule.forRoot('mongodb://localhost/lista'),
   ],
   controllers: [AppController],
